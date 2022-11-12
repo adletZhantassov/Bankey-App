f093be05da27cf3203f90a4f9a4ad40465ee9996
Adlet <adlet_1996@mail.ru>
2022-11-12 19:47:30 +0600

    Initial Commit

---
+++ Bankey App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey App
+//
+//  Created by Adlet Zhantassov on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
